04afff16c9d15015b753bdb034491ba41abced4e
Chris Parsons <chris.parsons@rackspace.com>
2014-01-05 18:44:18 -0600

    cleaned up challenge 1

--- challenge1.php
+++ challenge1.php
@@ -31,26 +31,22 @@ while($image = $imagelist -> next())
 }
 $imagenum = readline("Which image would you like to use? ");
 
-// find the image object in the list (i can't find a smarter way to get this image object)
-$imagelist = $RScompute -> imageList();
-while($image = $imagelist -> next())
-{
-    if ($image -> id == $imagelist[$imagenum] -> id)
-       $newimage = $image;
-}
+// create an image object for the chosen image
+$newimage = $RScompute -> Image($imagelist[$imagenum] -> id);
 
-// set the flavor object to the 512 MB flavor
+// create a flavor object for the 512 MB flavor
 $newflavor = $RScompute -> Flavor(2);
 
 // create the cloud server
 $newserver = $RScompute -> server();
 $newserver -> create(array(
-    'name'      => 'KrispyServ',
+    'name'      => 'KrispyServ2',
     'image'     => $newimage,
     'flavor'    => $newflavor));
 
-print "Server created.  Retreiving IP address and password information...\n";
-sleep(5);
+// sleep here because it takes a couple seconds for the cloud server object at RS to be created and assigned IPs and what not
+print "Server creation has begun.  Retreiving IP address and password information...\n";
+sleep(10);
 // place IP info of server in an array.  this is kinda messy but there doesn't seem to be direct access to the IP addresses
 $ips = $newserver -> ips();
 print "Public IPv4 Address: ".$ips -> public[0] -> addr."\n";
